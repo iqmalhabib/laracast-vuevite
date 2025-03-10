64470badff2d3d7d120a1e35f18f6d42b3e8cabe
iqmalhabib11 <iqmalhabib11@gmail.com>
2025-03-10 22:52:40 +0800

    ep18

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+
+let message = ref("hi test");
+
+    setTimeout(()=>{
+      message.value = "I have been changed";
+    },2000);
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <p><input type="text" v-model="message"/></p>
   </main>
 </template>
